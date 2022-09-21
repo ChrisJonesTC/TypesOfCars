c1797f9713fb485469567bd55fc5d6ba1399fb8d
ChrisJonesTC <cfjones91@gmail.com>
2022-09-21 09:21:43 -0400

    initial commit

---
+++ TypesOfCars/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace TypesOfCars
+{
+    internal class Car
+    {
+     public string Make { get; set; }
+     public string Model { get; set; }
+     public int Year { get; set; }
+
+    }
+}
---
+++ TypesOfCars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TypesOfCars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+
+            myCar.Make = "Kia";
+            myCar.Model = "Optima";
+            myCar.Year = 2016;
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
+        }
+    }
+}
